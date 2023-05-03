846e62ab5b580656832f2f2b62928f8b95f33336
Abbas Türkoğlu <abbasturkoglu@gmail.com>
2023-05-03 09:01:50 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+# The repository to add this issue to
+REPO_OWNER = 'abbasturkoglu'
+REPO_NAME = 'arxiv-papers-daily'
+
+# Set new submission url of subject
+NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
+
+# Keywords to search
+KEYWORD_LIST = ["feature fusion", "rgb-t tracking", "rgb-thermal", "rgb-thermal-tracking",
+                "rgb-t", "rgbt","rgb-t-fusion-tracking","rgb-tir"]
